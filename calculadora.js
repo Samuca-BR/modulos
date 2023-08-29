aab2931f29b0e5e801e3c93bc634ec61f996b951
arthur <arthur>
2023-08-29 11:54:07 -0300

    D,M,Sub

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,11 @@
 var somar = require("./modulos/somar");
+var subtrair =require("./modulos/subtrair");
 var media = require("./modulos/media");
+var dividir = require("./modulos/dividir");
+var multiplicar = require("./modulo/multiplicar");
 
 console.log(somar(20,90));
+console.log(subtrair(20,90));
 console.log(media(20,90));
+console.log(dividir(20,90));
+console.log(multiplicar(20,90));
