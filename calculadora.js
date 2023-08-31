d019843ad08df99e2d7dd671a5ee5460ee1c8752
Samuca-BR <sadetona3000@gmail.com>
2023-08-31 08:56:39 -0300

    31/8

--- calculadora.js
+++ calculadora.js
@@ -4,8 +4,8 @@ var media = require("./modulos/media");
 var dividir = require("./modulos/dividir");
 var multi = require("./modulos/multiplicar");
 
-console.log(somar(20,90));
-console.log(subtrair(20,90));
-console.log(media(20,90));
-console.log(dividir(20,90));
-console.log(multi(20,90));
+console.log("A soma de x e y é:"+somar(20,90));
+console.log("A subtração de x e y é:"+subtrair(20,90));
+console.log("A media de x e y é:"+media(20,90));
+console.log("O dividendo de x e y é:"+dividir(20,90));
+console.log("O produto de x e y é:"+multi(20,90));
